7f0e2234d89c38816a4784bb8f15dad9b2fe5507
Gevorji <techno9882@gmail.com>
2025-02-26 16:22:12 +0300

    Change attribute name from "location_name" to "name". Remove "name" key from attrs kwarg.

--- src/weatherapp/weather/forms.py
+++ src/weatherapp/weather/forms.py
@@ -4,11 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 class LocationSearchForm(forms.Form):
     template_name = 'weather/forms/location-search-form.html'
-    location_name = forms.CharField(
+    name = forms.CharField(
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control me-2', 'placeholder': _('Enter location name'),
-                'type': 'search', 'name': 'location'
+                'type': 'search'
             }
         )
     )
