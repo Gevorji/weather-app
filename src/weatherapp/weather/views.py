da3d2b0002777083963b99392287929ef9ccd524
Gevorji <techno9882@gmail.com>
2025-02-26 15:58:40 +0300

    Add logging into weather views

--- src/weatherapp/weather/views.py
+++ src/weatherapp/weather/views.py
@@ -1,4 +1,5 @@
 import contextlib
+import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import F
@@ -22,6 +23,9 @@ API_ERROR_MSG = getattr(settings, 'WEATHERAPP_EXTERNAL_API_ERROR_MSG', 'While wo
 NLOCATIONS_ON_PAGE = getattr(settings, 'NLOCATIONS_ON_PAGE', 5)
 
 
+logger = logging.getLogger(__name__)
+
+
 class IndexView(View):
     location_search_form = LocationSearchForm
 
@@ -37,6 +41,7 @@ class IndexView(View):
             users=ruser
         ).annotate(date_added=F('userlocation__date_added')).order_by('-date_added')
         tmpl_name = 'weather/index.html'
+        logger.debug('Fetched %s users (%s) locations', len(users_locations), ruser.username)
 
         paginator = Paginator(users_locations, NLOCATIONS_ON_PAGE)
         if page_num <= 0:
@@ -56,6 +61,7 @@ class IndexView(View):
             ]
         except OpenweathermapApiHTTPResponseError:
             messages.error(request, API_ERROR_MSG, fail_silently=True)
+            logger.exception('External service error while serving request for %s', request.user.username)
             return render(request, tmpl_name, {'location_search_form': self.location_search_form})
         return render(
             request, tmpl_name, {
@@ -82,6 +88,7 @@ class LocationsView(LoginRequiredMixin, View):
             ldtos = openweathermap.get_locations_by_name(lname)
         except OpenweathermapApiHTTPResponseError:
             messages.error(request, API_ERROR_MSG, fail_silently=True)
+            logger.exception('External service error while serving request for %s', request.user.username)
             return render(request, 'weather/locations.html', {'location_search_form': self.location_search_form})
 
         db_locations_list = list(Location.objects.filter(name__icontains=lname))
@@ -92,6 +99,11 @@ class LocationsView(LoginRequiredMixin, View):
             if (lati_rounder(dto.latitude), longi_rounder(dto.longitude))
             not in [(lati_rounder(loc.latitude), longi_rounder(loc.longitude)) for loc in db_locations_list]
         ]
+
+        logger.debug('%s request for locations with name "%s": '
+                     'received %s locations from DB and %s from API (%s were moved as they already in DB)',
+                     request.user.username, lname, len(db_locations_list),
+                     len(api_locations_list), len(db_locations_list) - len(api_locations_list))
 
         locations_list = api_locations_list + db_locations_list
         locations_list.sort(key=lambda m: m.name)
@@ -134,6 +146,12 @@ class LocationsView(LoginRequiredMixin, View):
 
             if not UserLocation.objects.filter(user=m2m_linkage.user, location=m2m_linkage.location).exists():
                 m2m_linkage.save()
+                logger.info(
+                    'User %s added location %s (id %s, %s, lat: %s, lon: %s)',
+                    request.user.username, selected_location_model.name,
+                    selected_location.id, selected_location.country,
+                    selected_location_model.latitude, selected_location.longitude,
+                )
         finally:
             with contextlib.suppress(KeyError):
                 del request.session['lately_requested_locations']
@@ -156,4 +174,10 @@ class LocationRemoveView(LoginRequiredMixin, View):
             UserLocation, user=request.user, location_id=user_location_id
         )
         m2m_linkage.delete()
+        logger.info(
+            'User %s removed location %s (id %s, %s, lat: %s, lon: %s)',
+            request.user.username, m2m_linkage.location.name,
+            m2m_linkage.location.id, m2m_linkage.location.country,
+            m2m_linkage.location.latitude, m2m_linkage.location.longitude,
+        )
         return HttpResponseRedirect('/')
