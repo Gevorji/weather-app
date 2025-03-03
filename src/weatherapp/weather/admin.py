68105ef2a425e1d6571c7b6362c292e981684069
Gevorji <techno9882@gmail.com>
2025-03-03 21:01:38 +0300

    Add admin interface to weather models.

--- src/weatherapp/weather/admin.py
+++ src/weatherapp/weather/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location, UserLocation
+
+class UserLocationInline(admin.TabularInline):
+    list_display = ['user', 'date_added']
+    model = UserLocation
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'latitude', 'longitude', 'country']
+    inlines = [UserLocationInline]
+
+class UserLocationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'location', 'date_added']
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(UserLocation, UserLocationAdmin)
