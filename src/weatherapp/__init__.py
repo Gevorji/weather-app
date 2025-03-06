5acba184e2c21b08d0eb4f8256aa277f837368d8
Gevorji <techno9882@gmail.com>
2025-03-06 15:15:27 +0300

    Move logging configuration to settings via LOGGING attr.

--- src/weatherapp/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from django.conf import settings
-
-logging.basicConfig(
-    level=getattr(settings, 'LOG_LEVEL', logging.INFO),
-    format='%(asctime)s %(name)s (%(levelname)s): %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
