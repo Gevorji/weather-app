89e8e2c3227dc9b152213f67bad6fa18901a8d22
Gevorji <techno9882@gmail.com>
2025-03-10 22:29:11 +0300

    Make ALLOWED_HOSTS django setting env dependent.

--- src/weatherapp/weather_project/settings.py
+++ src/weatherapp/weather_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", 0))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
